a6c955c3217c5df619a395ae2d9ec94e2307f596
Creeper121-newacc <ahsan.yunus19@gmail.com>
2022-12-08 12:33:58 +0600

    MORE INSANITY

--- js/youtubeBot.js
+++ js/youtubeBot.js
@@ -5,36 +5,38 @@ function getElementByXpath(path) {
 
 $(document).ready(function() {
     $('.youtube-stream').ready(function() {
-        // these are required, no exclusions
-        // the textbox in which u type stuff
-        const textbox = getElementByXpath("//div[@id='input']");
+        $('.youtube-stream').load(function() {
+            // these are required, no exclusions
+            // the textbox in which u type stuff
+            const textbox = getElementByXpath("//div[@id='input']");
 
-        // the little paper plane button u click to send what u wrote in the textbox
-        const sendButton = getElementByXpath("//button[@aria-label='Send']");
+            // the little paper plane button u click to send what u wrote in the textbox
+            const sendButton = getElementByXpath("//button[@aria-label='Send']");
 
-        window.addEventListener("keydown", function(event) {
-            const key = event.key.toLowerCase();
-            if (!event.repeat) {
-                let command = "";
-                if (key == "w") command = "up";
-                else if (key == "a") command = "left";
-                else if (key == "s") command = "down";
-                else if (key == "d") command = "right";
-                else if (key == "1") command = "start";
-                else if (key == "2") command = "select";
-                else if (key == "j") command = "A";
-                else if (key == "k") command = "B";
-                if (command != "") {
-                    textbox.focus();
-                    document.execCommand("insertHTML", false, command);
-                    sendButton.click();
-                    command = "";
-                    textbox.textContent = "";
-                    setTimeout(function() {
+            window.addEventListener("keydown", function(event) {
+                const key = event.key.toLowerCase();
+                if (!event.repeat) {
+                    let command = "";
+                    if (key == "w") command = "up";
+                    else if (key == "a") command = "left";
+                    else if (key == "s") command = "down";
+                    else if (key == "d") command = "right";
+                    else if (key == "1") command = "start";
+                    else if (key == "2") command = "select";
+                    else if (key == "j") command = "A";
+                    else if (key == "k") command = "B";
+                    if (command != "") {
+                        textbox.focus();
+                        document.execCommand("insertHTML", false, command);
+                        sendButton.click();
+                        command = "";
                         textbox.textContent = "";
-                    });
+                        setTimeout(function() {
+                            textbox.textContent = "";
+                        });
+                    }
                 }
-            }
+            });
         });
     });
 });
